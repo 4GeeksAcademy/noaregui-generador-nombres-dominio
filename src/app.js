864b6af240e6541fca293a9356023cfa4e148b0b
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-03-06 20:30:48 +0000

    generar dominio

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let generarDominio = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let com = [".com", ".es"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    adj[adjIndex] +
+    " " +
+    noun[nounIndex] +
+    " " +
+    com
+  );
+};
